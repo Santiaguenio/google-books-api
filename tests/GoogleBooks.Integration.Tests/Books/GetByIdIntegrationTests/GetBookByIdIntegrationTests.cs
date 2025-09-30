613c3bb1d90e4b8fd6163cc1c409f57267b61266
Santiaguenio <aloatias88@gmail.com>
2025-09-30 23:46:06 +0200

    Add httpClient timeout tests

--- tests/GoogleBooks.Integration.Tests/Books/GetByIdIntegrationTests/GetBookByIdIntegrationTests.cs
+++ tests/GoogleBooks.Integration.Tests/Books/GetByIdIntegrationTests/GetBookByIdIntegrationTests.cs
@@ -82,6 +82,36 @@ public class GetBookByIdIntegrationTests
     }
 
     [Fact]
+    public async Task Should_ReturnInternalServerError_WhenExternalServerTimeoutOccurs()
+    {
+        // arrange
+        var bookId = "s1gVAAAAYAAJ";
+
+        var expectedHttpResult = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+        {
+            Content = new StringContent(await File.ReadAllTextAsync("Books/GetByIdIntegrationTests/ExternalServerError/ExpectedExternalServerErrorResult.json"))
+        };
+
+        _testFactory.SetMockedHttpClientFactory(_testFactory.GetGoogleBooksUrl);
+        _testFactory.MockedHttpMessageHandler
+               .Protected()
+               .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(_ => _.Method == HttpMethod.Get && _.RequestUri!.AbsoluteUri.Equals($"{_testFactory.GetGoogleBooksUrl}volumes/{bookId}")),
+                    ItExpr.IsAny<CancellationToken>())
+                .ThrowsAsync(new TaskCanceledException());
+
+        _client.Timeout = TimeSpan.FromMilliseconds(1500);
+
+        // act
+        var actualHttpResult = await _client.GetAsync($"books/{bookId}");
+
+        // assert
+        Assert.Equal(expectedHttpResult.StatusCode, actualHttpResult.StatusCode);
+        Assert.Equivalent(await expectedHttpResult.Content.ReadFromJsonAsync<BookFullDto>(), await actualHttpResult.Content.ReadFromJsonAsync<BookFullDto>());
+    }
+
+    [Fact]
     public async Task Should_ReturnInternalServerError_WhenHttpClientThrowsException()
     {
         // arrange
