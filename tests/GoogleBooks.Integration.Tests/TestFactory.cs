0c4f856a01146f2ee5b6f976cc06d38ced2003d6
Santiaguenio <aloatias88@gmail.com>
2025-10-02 11:16:00 +0200

    Add Mocks reseting

--- tests/GoogleBooks.Integration.Tests/TestFactory.cs
+++ tests/GoogleBooks.Integration.Tests/TestFactory.cs
@@ -21,9 +21,13 @@ public class TestFactory : WebApplicationFactory<Program>
     }
 
     internal string GetGoogleBooksUrl => "https://www.googleapis.com/books/v1/";
-
     internal Mock<IHttpClientFactory> MockedHttpClientFactory { get; private set; } = new();
     internal Mock<HttpMessageHandler> MockedHttpMessageHandler { get; private set; } = new();
+
+    internal void ResetMocks()
+    {
+        MockedHttpMessageHandler.Reset();
+    }
 
     internal void SetMockedHttpClientFactory(string url = "https://default-test-url.com")
     {
