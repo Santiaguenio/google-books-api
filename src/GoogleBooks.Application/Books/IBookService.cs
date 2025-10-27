8370fada4d740eac3db4c12026a8e0e4b1d00bc2
Santiaguenio <aloatias88@gmail.com>
2025-10-27 19:57:15 +0100

    Parse DTOs to domain models on requests

--- src/GoogleBooks.Application/Books/IBookService.cs
+++ src/GoogleBooks.Application/Books/IBookService.cs
@@ -1,11 +1,11 @@
-﻿using GoogleBooks.Application.Common.Services;
+﻿using GoogleBooks.Application.Books.Models;
+using GoogleBooks.Application.Common.Services;
 using GoogleBooks.Contracts;
-using GoogleBooks.Contracts.Requests.Books;
 using GoogleBooks.Contracts.Responses.Books;
 
 namespace GoogleBooks.Application.Books;
 
 public interface IBookService : IService<BookFullDto>
 {
-    Task<EntitiesByCriteriaDto<BookFullDto>> ListByKeyWordsAsync(PageParams request, CancellationToken cancellationToken);
+    Task<EntitiesByCriteriaDto<BookFullDto>> ListByKeyWordsAsync(ListByKeyWordsParams request, CancellationToken cancellationToken);
 }
--- src/GoogleBooks.WebApi/Controllers/BooksController.cs
+++ src/GoogleBooks.WebApi/Controllers/BooksController.cs
@@ -29,8 +29,8 @@ public class BooksController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
     public async Task<IActionResult> ListByKeyWordsAsync(
-        [FromServices] IListByCriteria<PageParams> listByCriteria,
-        [FromQuery] PageParams pageParams,
+        [FromServices] IListByCriteria<PageParamsDto> listByCriteria,
+        [FromQuery] PageParamsDto pageParams,
         CancellationToken cancellationToken)
     {
         return Ok(await listByCriteria.DoAsync(pageParams, cancellationToken));
